a8c4d62902aeece2861b697cbe0196d60e204501
thaborak <thaborak@gmail.com>
2018-02-26 17:27:26 -0500

    comment cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,7 @@
 
 // eslint-disable-next-line no-unused-vars
 'use strict';
-/*const store = {
-  items: [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
-  ],
-  hideCheckedItems: false,
-  searchTerm: ''
-};
-*/
+
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
